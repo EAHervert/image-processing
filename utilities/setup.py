89bed84faa2b998643148467ca72e51d59c96244
Esau A. Hervert Hernandez <eah170630@utdallas.edu>
2024-12-17 15:02:02 -0600

    2024-12-17 -- Added spatial-filtering

---
+++ utilities/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='utilities',
+      version='0.0.1',
+      description='methods and transforms for image denoising and analysis',
+      author='Esau Hervert Hernandez',
+      author_email='eah170630@utdallas.edu'
+      )
